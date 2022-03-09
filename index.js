07f98c8253f94775c0ae0954c886f6b33ed9ae2c
정성원 <tuenkle@jeongseong-won-ui-MacBookPro.local>
2022-03-10 02:26:39 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    let API_KEY = "AIzaSyDQYEGlAf3XQ1_tPFsCjnWlaOc2OtxWpho"
+
+    let video = ''
+    $("form").submit(function(event){
+        event.preventDefault()
+
+        let search = $("#search").val()
+        videoSearch(API_KEY, search, 10)
+    })
+    function videoSearch(key, search, maxResults){
+
+        $("#videos").empty()
+        
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key 
+        + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data){
+            console.log(data)
+            data.items.forEach(item => {
+                video = `
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder"0" allowfullscreen></iframe>
+                `
+                $("#videos").append(video)
+            })
+        })
+    }
+})
---
+++ index2.js
@@ -0,0 +1,55 @@
+const API_KEY = "AIzaSyDlGBFG5hefazM8lQgcc57-FhhC9YwUXdU";
+const iframe1 = document.getElementById("iframe1");
+const iframe2 = document.getElementById("iframe2")
+const iframe3 = document.getElementById("iframe3")
+const iframe4 = document.getElementById("iframe4")
+const iframe5 = document.getElementById("iframe5")
+const iframe6 = document.getElementById("iframe6")
+const iframe7 = document.getElementById("iframe7")
+const iframe8 = document.getElementById("iframe8")
+const iframe9 = document.getElementById("iframe9")
+const iframe10 = document.getElementById("iframe10")
+const submit = document.getElementById("submit");
+const searchName = document.getElementById("search");
+
+function search(key, search, maxResults){
+    let url = "https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search
+    let i = 0
+    fetch(url).then((response) => response.json()).then((data) => data.items.forEach(item => {
+        switch (i) {
+            case 1:
+                iframe1.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 2:
+                iframe2.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 3:
+                iframe3.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 4:
+                iframe4.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 5:
+                iframe5.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 6:
+                iframe6.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 7:
+                iframe7.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 8:
+                iframe8.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 9:
+                iframe9.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            case 10:
+                iframe10.src = `http://www.youtube.com/embed/${item.id.videoId}`
+            default:
+                break;
+        }
+        i += 1
+    }));
+}
+
+submit.onclick = function(){
+    search(API_KEY, searchName.value, 10)
+}
+document.addEventListener("keyup", function(e){
+    console.log(e.key)
+    if(e.key === "Enter"){
+        search(API_KEY, searchName.value, 10)
+    }
+})
